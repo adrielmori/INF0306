5d656c3ff78f2d110b5743251186d6c2322be9d5
adrielmori <adrielmori@outlook.com>
2024-01-25 10:21:30 -0300

    Ajustando o Layout

--- app/src/main/java/com/ufg/cardiwatch/MainActivity.java
+++ app/src/main/java/com/ufg/cardiwatch/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
+
+    public void monitoringActivity(View view) {
+        Intent intent = new Intent(this, MonitoringActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/ufg/cardiwatch/MonitoringActivity.java
@@ -0,0 +1,15 @@
+package com.ufg.cardiwatch;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MonitoringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitoring);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
