a045800e0c8b2f06c6eb3dc2a48081004a415b72
igorpadua <igorpadua@tutanota.com>
2024-01-25 14:38:49 -0300

    add: conectar conta da google

--- app/src/main/java/com/ufg/cardiwatch/MainActivity.java
+++ app/src/main/java/com/ufg/cardiwatch/MainActivity.java
@@ -1,17 +1,56 @@
 package com.ufg.cardiwatch;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.DataReadRequest;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+
+   private FitnessOptions fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
+            .addDataType(DataType.TYPE_HEART_RATE_BPM, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.AGGREGATE_HEART_RATE_SUMMARY, FitnessOptions.ACCESS_WRITE)
+            .build();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInAccount account = GoogleSignIn.getAccountForExtension(this, fitnessOptions);
+
+        if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    1, // e.g. 1
+                    account,
+                    fitnessOptions);
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                accessGoogleFit();
+            }
+        }
     }
 
     public void profileActivity(View view) {
@@ -22,5 +61,33 @@ public class MainActivity extends AppCompatActivity {
     public void monitoringActivity(View view) {
         Intent intent = new Intent(this, MonitoryActivity.class);
         startActivity(intent);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void accessGoogleFit() {
+        LocalDateTime end = LocalDateTime.now();
+        LocalDateTime start = end.minusYears(1);
+        long endSeconds = end.atZone(ZoneId.systemDefault()).toEpochSecond();
+        long startSeconds = start.atZone(ZoneId.systemDefault()).toEpochSecond();
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .setTimeRange(startSeconds, endSeconds, TimeUnit.SECONDS)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .build();
+
+        TextView textView = findViewById(R.id.textViewName);
+        GoogleSignInAccount account = GoogleSignIn.getAccountForExtension(this, fitnessOptions);
+        Fitness.getHistoryClient(this, account)
+                .readData(readRequest)
+                .addOnSuccessListener(response -> {
+                    Log.i(TAG, "OnSuccess()");
+                })
+                .addOnFailureListener(
+                        e -> {
+                            Log.i(TAG, "OnFailure()", e);
+                        }
+                );
     }
 }
