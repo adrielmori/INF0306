8d7846ba55914f13f565c1afba65d21012fd1be8
igorpadua <igorpadua@tutanota.com>
2024-01-26 14:26:10 -0300

    add: imagem profile

--- app/src/main/java/com/ufg/cardiwatch/MainActivity.java
+++ app/src/main/java/com/ufg/cardiwatch/MainActivity.java
@@ -3,7 +3,6 @@ package com.ufg.cardiwatch;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.RequiresApi;
