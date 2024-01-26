267bfac0f881a289c4cd67370a40402e55b965b5
igorpadua <igorpadua@tutanota.com>
2024-01-26 15:45:25 -0300

    add: envia dados para o mqtt

--- app/src/main/java/com/ufg/cardiwatch/MonitoryActivity.java
+++ app/src/main/java/com/ufg/cardiwatch/MonitoryActivity.java
@@ -1,14 +1,32 @@
 package com.ufg.cardiwatch;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.gson.Gson;
+import com.ufg.cardiwatch.model.Pessoa;
+import com.ufg.cardiwatch.util.Mqtt;
 
 public class MonitoryActivity extends AppCompatActivity {
+
+    Pessoa pessoa = new Pessoa();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitory);
+
+        Intent intent = getIntent();
+        pessoa = (Pessoa) intent.getSerializableExtra("pessoa");
+    }
+
+    public void enviaParaMqtt(View view) {
+        Gson gson = new Gson();
+        String json = gson.toJson(pessoa);
+
+        Mqtt.publishMessage("cardiwatch", json);
     }
 }
