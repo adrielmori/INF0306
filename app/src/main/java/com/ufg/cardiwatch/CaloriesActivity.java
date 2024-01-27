04bb14f1916fd7ba58ed492a81b47b060c902bd5
igorpadua <igorpadua@tutanota.com>
2024-01-27 16:39:15 -0300

    add: CaloriesActivity create

---
+++ app/src/main/java/com/ufg/cardiwatch/CaloriesActivity.java
@@ -0,0 +1,14 @@
+package com.ufg.cardiwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaloriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calories);
+    }
+}
